3bd089665a7a6dd4c94efd4a8e6d8345fc9ddfa2
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-07-01 17:32:14 -0300

    enviando exercício typescript

---
+++ exercicio.ts
@@ -0,0 +1,14 @@
+function multiplicar (valor1: number, valor2:number){
+    
+    return valor1 * valor2;
+}
+
+console.log(multiplicar(40,15));
+
+
+function saudacao (nome : string){
+    
+    return 'Olá, ' + nome;
+}
+
+console.log(saudacao("Állan"));
